598e70fff80274675646856e2a4c6be0cc823ef2
Animeyshnik <davidazizzyan@gmail.com>
2024-10-08 05:23:35 -0400

    CppCmake

---
+++ build/CMakeFiles/HomeHello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeHello.dir/project.cpp.o"
+  "CMakeFiles/HomeHello.dir/project.cpp.o.d"
+  "HomeHello"
+  "HomeHello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeHello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/HomeHello.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HomeHello.
+# This may be replaced when dependencies are built.
